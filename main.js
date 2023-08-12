63ff0b91e807ee687ca65466a55ec1241669f2ec
ferney <ferney.montoyag@gmail.com>
2023-08-12 14:13:38 -0500

    First mobile version finished

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 /**
  * Getting the necesary HTML elements
  */
-const btn = document.querySelector('button');
+const faqsDiv = document.querySelector('.faqs');
+const paragraphsHidden = document.querySelectorAll('.answer-hidden--paragraph');
+const questions = document.querySelectorAll('.question--paragraph');
+const paragraphsHiddenArray = [...paragraphsHidden];
+const questionsArray = [...questions];
+
+faqsDiv.addEventListener('click', (e) => {
+  
+    const btnName = e.target.tagName;
+    const btn = e.target;
+
+    if(btnName === 'BUTTON') {
+        btn.classList.toggle('button__active');
+
+        btnNumber = Number(btn.className[1]);
+        
+        paragraphsHiddenArray[btnNumber - 1].classList.toggle('toggle');
+        questionsArray[btnNumber - 1].classList.toggle('question__active');
+    }
+
+})
